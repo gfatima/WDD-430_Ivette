c4cba22811e2e925d4813f1dcfad6f65d8a236c8
Ivette-syta <37168871+gfatima@users.noreply.github.com>
2022-01-29 20:25:49 -0500

    version 4.1

--- cms/src/app/documents/document-detail/document-detail.component.ts
+++ cms/src/app/documents/document-detail/document-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-detail',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentDetailComponent implements OnInit {
 
+  @Input() document: Document;
+
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
--- cms/src/app/documents/document-item/document-item.component.ts
+++ cms/src/app/documents/document-item/document-item.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-item',
   templateUrl: './document-item.component.html',
   styleUrls: ['./document-item.component.css']
 })
+
 export class DocumentItemComponent implements OnInit {
 
+  @Input() document: Document;
+  @Output() selectedContact = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
